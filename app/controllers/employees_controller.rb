97f568688dd2a3bbf2aaedba05625fbeb770fe90
Lucas Felipe <lucasfelipedepaiva2@gmail.com>
2023-06-24 17:11:13 -0300

    Adjustments

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -35,7 +35,9 @@ class EmployeesController < ApplicationController
 
   def destroy
     @employee = Employee.find_by(id: params[:id])
+
     @employee.destroy
+
     redirect_to employees_path, notice: 'Funcionário excluído com sucesso!'
   end
 
